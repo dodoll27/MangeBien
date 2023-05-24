dec1ae73892c31c9ddb0299bfc407cb61f50806f
Teo <teo.botezatu@yahoo.com>
2023-05-24 18:39:29 +0200

    improved :)

---
+++ src/components/Register.js
@@ -0,0 +1,66 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { useForm } from "react-hook-form";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+import { useState } from "react";
+
+export const Register = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
+  const from = location.state?.from?.pathname || "/";
+
+  function onSubmit(data) {
+    auth.signin(data.username, () => {
+      // Send them back to the page they tried to visit when they were
+      // redirected to the login page. Use { replace: true } so we don't create
+      // another entry in the history stack for the login page.  This means that
+      // when they get to the protected page and click the back button, they
+      // won't end up back on the login page, which is also really nice for the
+      // user experience.
+      navigate(from, { replace: true });
+    });
+  }
+
+  return (
+    <div className="auth-container">
+      <div className="wrapper">
+        <div className="auth-form-box">
+          <form className="signin-form">
+            <h2>Sign Up</h2>
+            <div className="auth-input-group">
+              <div className="auth-input-field">
+                <i className="fa-solid fa-lock"></i>
+                <input type="text" placeholder="Name" name="name" />
+              </div>
+              <div className="auth-input-field">
+                <i className="fa-regular fa-envelope"></i>
+                <input type="text" placeholder="E-mail" name="email" />
+              </div>
+              <div className="auth-input-field">
+                <i className="fa-solid fa-lock"></i>
+                <input type="password" placeholder="Password" name="password" />
+              </div>
+              <div className="auth-input-field">
+                <i className="fa-solid fa-lock"></i>
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="password"
+                />
+              </div>
+            </div>
+            <div className="auth-input-field">
+              <button className="signup-button" type="submit">
+                Sign Up
+              </button>
+            </div>{" "}
+            <a className="login-link" href="/login">
+              Log In
+            </a>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
